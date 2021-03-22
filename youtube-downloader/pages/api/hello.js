34cdcd8dcfff18b7fc828d0949a63986a3417e9b
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2021-03-22 11:26:08 +0530

    Add -ytdownload api

--- youtube-downloader/pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
-// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
-// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
-
-ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ')
-  .pipe(fs.createWriteStream('video.mp4'));
--- youtube-downloader/pages/index.js
+++ youtube-downloader/pages/index.js
@@ -1,28 +1,54 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState } from "react";
+import Head from "next/head";
 
 export default function Home() {
+  const [url, setUrl] = useState("");
+
+  const handleClick = async() => {
+    const response = await fetch("http://localhost:3000/api/yt");
+    // var a = document.createElement('a');
+  	// 	a.href = `http://localhost:3000/video.mp4`;
+  	// 	a.setAttribute('download', '');
+		// a.click();
+    console.log(response);
+  };
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Youtube downloader by Rams</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 bg-white border-b border-gray-200">
+              <div className="mb-4">
+                <label className="text-xl text-gray-600">
+                  Paste the youtube link
+                </label>
+                <input
+                  type="text"
+                  className="border-2 border-gray-300 p-2 w-full"
+                  name="title"
+                  id="title"
+                  value={url}
+                  onChange={(e)=>setUrl(e.target.value)}
+                  required
+                ></input>
+              </div>
+            </div>
+            <button
+              className="p-3 flex w-full justify-center bg-blue-500 text-white hover:bg-blue-400"
+              required
+              onClick={() => handleClick()}
+            >
+              Download Video
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
