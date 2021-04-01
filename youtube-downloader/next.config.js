871ca533c95f0610ed7abef5a02ea7204206b3c4
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2021-04-01 23:40:47 +0530

    Update - next js api key

---
+++ youtube-downloader/next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        APIKEY: "AIzaSyC1C3QC9O-clTbukc7ZrbbtjCU9mtrcy5E",
+        AUTHDOMAIN: "download-644dd.firebaseapp.com",
+        PROJECTID: "download-644dd",
+        STORAGEBUCKET: "download-644dd.appspot.com",
+        MESSAGINGSENDERID: "193197359752",
+        APPID: "1:193197359752:web:98708cafb2728c98d5953d"
+    }
+}
