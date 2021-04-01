3cb76bb462e8e94443de1dfd243b88514cacc718
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2021-04-02 00:26:34 +0530

    Fix - download ui

--- youtube-downloader/pages/index.js
+++ youtube-downloader/pages/index.js
@@ -16,27 +16,32 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Youtube downloader by Rams</title>
+        <title>YT downloader by Rams</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="py-12">
+      <div className="py-12 flex justify-center">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <h1 className="text-xl"> YT downloader </h1>
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 bg-white border-b border-gray-200">
               <div className="mb-4">
-                <label className="text-xl text-gray-600">
+                <label className="text-xl text-gray-600 justify-center">
                   Paste the youtube link
                 </label>
-                <input
+              </div>
+              <div className="flex">
+              <input
                   type="text"
                   className="border-2 border-gray-300 p-2 w-full"
                   name="title"
                   id="title"
                   value={url}
                   onChange={(e)=>setUrl(e.target.value)}
+                  placeholder="Paste the youtube link"
                   required
                 ></input>
+                <button className="p-3 flex w-full  bg-blue-500 text-white hover:bg-blue-400"> Convert video </button>
               </div>
             </div>
             <button
