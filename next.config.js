314219da5103ffb33d666588709e918634e2daba
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2021-04-13 22:55:51 +0530

    Update - icon 144 pixel

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+    }
+})
+
 module.exports = {
     env: {
         YOUTUBEAPI: "AIzaSyB8Fk-MWT_r8nVgG35gIZoP-DhJYpJ_tZ0"
