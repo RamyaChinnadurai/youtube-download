8a3d635b7903871a5da326430e44029a5f04f4c9
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2021-04-14 23:33:13 +0530

    Update - audit fixes

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"
         />
         <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />
