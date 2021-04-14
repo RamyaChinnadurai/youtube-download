3cc596352da488102b11c40224904f9d8d7988b7
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2021-04-14 23:10:51 +0530

    Update - service worker

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,3 @@
-const CACHE_NAME = "simple-cache-v1";
-const urlsToCache = ["/"];
-
-self.addEventListener("install", event => {
-    const preLoaded = caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(urlsToCache))
-    event.waitUntil(preLoaded);
-});
 
 self.addEventListener("fetch", event => {
     const response = caches.match(event.request)
